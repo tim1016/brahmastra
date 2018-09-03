43ba74beaf0ef90b3570535c3bf9418f60a769b3
tim1016 <inkant@gmail.com>
2018-09-03 17:07:51 -0500

    Removed the reitest plugin. and everything is through ReiPro

--- rei/wp-content/plugins/reitest/templates/chat.php
+++
@@ -1,3 +0,0 @@
-<div class="wrap">
-    <h1>Settings</h1>
-</div>
--- rei/wp-content/plugins/reitest/templates/testimonial.php
+++
@@ -1,6 +0,0 @@
-<div class="wrap">
-    <h1>Testimonial shortcodes</h1>
-    <p>Testimonial Form Shortcode</p>
-    <p><code>[testimonial-form]</code></p>
-    <p><code>[testimonial-slideshow]</code></p>
-</div>    
